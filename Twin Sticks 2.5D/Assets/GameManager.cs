33ed08b89b069fd0a84f7fc852f970c719157831
FBSALOMON <fbsalomon@gmail.com>
2018-08-01 15:40:28 -0400

    Twin Sticks 2.5D(21/21)

---
+++ Twin Sticks 2.5D/Assets/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameManager : MonoBehaviour {
+
+    public bool recording;
+
+    private bool gamePause = false;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (CrossPlatformInputManager.GetButton("Fire1"))
+        {
+            recording = false;
+        } else
+        {
+            recording = true;
+        }
+
+        if (CrossPlatformInputManager.GetButtonDown("Pause") && !gamePause)
+        {
+            PauseGame();
+        } else if (CrossPlatformInputManager.GetButtonDown("Pause") && gamePause)
+        {
+            ResumeGame();
+        }
+	}
+
+    void PauseGame()
+    {
+        Time.timeScale = 0f;
+        Time.fixedDeltaTime = 0f;
+        gamePause = true;
+    }
+    void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = 0.02f;
+        gamePause = false;
+    }
+    /*private void OnApplicationPause(bool pause)
+    {
+        gamePause = pause;
+        PauseGame();
+    }*/
+}
---
+++ Twin Sticks 2.5D/Assets/ReplaySystem.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaySystem : MonoBehaviour {
+
+    public const int BUFFERFRAMES = 100;
+    public MyKeyFrame[] keyFrames = new MyKeyFrame[BUFFERFRAMES];
+    public new Rigidbody rigidbody;
+    GameManager gameManager;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(gameManager.recording)
+        {
+            Record();
+        } else
+        {
+            PlayBack();
+        }
+        
+    }
+
+    private void Record()
+    {
+        rigidbody.isKinematic = false;
+        int frame = Time.frameCount % BUFFERFRAMES;
+        float time = Time.time;
+
+        keyFrames[frame] = new MyKeyFrame(time, transform.position, transform.rotation);
+    }
+
+    private void PlayBack()
+    {
+        rigidbody.isKinematic = true;
+        int frame = Time.frameCount % BUFFERFRAMES;
+        transform.position = keyFrames[frame].position;
+        transform.rotation = keyFrames[frame].rotation;
+    }
+}
+
+/// <summary>
+/// A structure for storing time, rotation and position.
+/// </summary>
+public struct MyKeyFrame {
+    public float frameTime;
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public MyKeyFrame(float aTime, Vector3 aPosition, Quaternion aRotation)
+    {
+        frameTime = aTime;
+        position = aPosition;
+        rotation = aRotation;
+    }
+}
